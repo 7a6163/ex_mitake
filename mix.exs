89b8124409825612893d67af26e13cbaa97325ef
Zac <mymengyu@gmail.com>
2018-08-16 16:15:51 +0800

    Add credo

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule ExMitake.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.2.0"},
-      {:ex_doc, "~> 0.18", only: :dev}
+      {:ex_doc, "~> 0.18", only: :dev},
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
     ]
   end
 
