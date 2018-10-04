3debb483025bb40db0a495a3f6b548385ef6f3fc
Zac <mymengyu@gmail.com>
2018-10-04 16:58:51 +0800

    Upgrade httpoison

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule ExMitake.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.2.0"},
+      {:httpoison, "~> 1.0"},
       {:ex_doc, "~> 0.18", only: :dev},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
     ]
